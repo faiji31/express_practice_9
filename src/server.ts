23ecbedce9aca2e95754ce29594d2579424dd230
Faiji Akbar Liam <faijiakbarliam2016@gmail.com>
2026-08-16 04:18:59 +0600

    2nd commit

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,28 @@
 import express, { type Application, type Request, type Response } from 'express'
+import {Pool} from 'pg'
 const app:Application = express()
 const port = 5000
 
+
+app.use(express.json())
+
+
+const pool = new Pool(
+    {
+        connectionString:"postgresql://neondb_owner:npg_iMYaPWXm98tU@ep-orange-king-ayvuyane-pooler.c-5.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+    }
+)
+
 app.get('/', (req:Request, res:Response) => {
-  res.send('server is running!')
+//   res.send('server is running!')
+  res.status(200).json({
+        success:true,
+        message:"next level"
+       
+    })
 })
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
