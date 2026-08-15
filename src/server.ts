075516451fbc3bbaa7b6855b8f588411f0465f5f
Faiji Akbar Liam <faijiakbarliam2016@gmail.com>
2026-08-16 04:58:40 +0600

    5th commit

--- src/server.ts
+++ src/server.ts
@@ -76,8 +76,58 @@ app.post('/api/users',async(req:Request,res:Response)=>{
     }
 })
 
-app.get('api/users.')
+app.get('/api/users',async(req:Request,res:Response)=>{
+    try {
+        const result = await pool.query(
+            `SELECT * FROM users
+            `
+        )
+         res.status(201).json({
+                success:true,
+                message:"user retrived successfully!",
+                data:result.rows
+            })
 
+        
+    } catch (error:any) {
+        res.status(500).json({
+            success:false,
+            message:error.message,
+            error:error
+        })
+        
+    }
+})
+app.get('/api/users/:id',async(req:Request,res:Response)=>{
+    const {id} = req.params
+    try {
+        const result = await pool.query(
+        `SELECT * FROM users WHERE id=$1`
+    ,[id])
+
+    if(result.rows.length ===0){
+        res.status(404).json({
+            success:false,
+            message:"user is not found!",
+            data:{}
+        })
+       
+    }
+     res.status(201).json({
+            success:true,
+                message:"user retrived successfully!",
+                data:result.rows[0]
+
+        })
+    } catch (error:any) {
+         res.status(500).json({
+            success:false,
+            message:error.message,
+            error:error
+        })
+        
+    }
+})
 
 
 app.listen(port, () => {
