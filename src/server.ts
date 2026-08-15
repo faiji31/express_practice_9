95ceb370607f6f6e8767229f9e03f510000f10e1
Faiji Akbar Liam <faijiakbarliam2016@gmail.com>
2026-08-16 04:44:53 +0600

    4th commit

--- src/server.ts
+++ src/server.ts
@@ -20,9 +20,10 @@ const initDB=async()=>{
         await pool.query(`
             CREATE TABLE IF NOT EXISTS users(
             id SERIAL PRIMARY KEY,
-            name VARCHAR(20) NOT NULL,
-            email VARCHAR(20) NOT NULL,
+            name VARCHAR(255) NOT NULL,
+            email VARCHAR(255) UNIQUE NOT NULL,
             password VARCHAR(20) NOT NULL,
+            is_active BOOLEAN DEFAULT true,
             age INT,
 
             created_at TIMESTAMP DEFAULT NOW(),
@@ -49,6 +50,33 @@ app.get('/', (req:Request, res:Response) => {
        
     })
 })
+
+app.post('/api/users',async(req:Request,res:Response)=>{
+
+    const{name,age,password,email} = req.body
+    try {
+        const result = await pool.query(`
+            INSERT INTO users(name,age,password,email) VALUES($1,$2,$3,$4) RETURNING *
+            
+            `,[name,age,password,email])
+
+            res.status(201).json({
+                success:true,
+                message:"user created successfully!",
+                data:result.rows[0]
+            })
+        
+    } catch (error:any) {
+        res.status(500).json({
+            success:false,
+            message:error.message,
+            error:error
+        })
+        
+    }
+})
+
+app.get('api/users.')
 
 
 
